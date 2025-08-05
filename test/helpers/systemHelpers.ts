d70efe0e8724b2a59b70eeff16f5207e54fb5b85
Zachary Kurmas <kurmasz@gvsu.edu>
2025-08-05 14:59:21 -0400

    I am confused

--- test/helpers/systemHelpers.ts
+++ test/helpers/systemHelpers.ts
@@ -11,22 +11,31 @@ import { Workbench, Notification, NotificationType } from 'vscode-extension-test
 import { expect } from 'chai';
 
 export async function waitForNotification(type: NotificationType, matcher: (str: string) => boolean, timeout = 4000) {
-    const center = await new Workbench().openNotificationsCenter();
+    let center = await new Workbench().openNotificationsCenter();
     const start = Date.now();
     let notifications: Notification[] = [];
     let messages: string[] = [];
     while (Date.now() - start < timeout) {
         notifications = await center.getNotifications(type);
         messages = await Promise.all(notifications.map(async (n) => n.getMessage()));
+        console.log(`Elapsed ${Date.now() - start}`);
+        console.log(messages);
         const matches = messages.filter(matcher);
         expect(matches.length).to.be.at.most(1);
         if (matches.length === 1) {
             return matches[0];
         }
         await new Promise(resolve => setTimeout(resolve, 500));
+        center = await new Workbench().openNotificationsCenter();
     } // end while
 
-    await center.clearAllNotifications();
+    console.log("About to give up");
+    await logAllNotifications();
+    console.log("-----");
+
+    if (notifications.length > 0) {
+        await center.clearAllNotifications();
+    }
     if (notifications.length === 0) {
         expect.fail('No notifications appeared.');
     } else {
--- test/system/00-commandWithNoFolder.test.ts
+++ test/system/00-commandWithNoFolder.test.ts
@@ -3,6 +3,7 @@
 
 import {Workbench, NotificationType, WebDriver, VSBrowser } from 'vscode-extension-tester';
 import { logAllNotifications, waitForNotification } from '../helpers/systemHelpers';
+import { logFileName } from '../../src/fileLogger';
 
 describe('Behavior with no folder open', function() {
 	let driver: WebDriver;
@@ -20,12 +21,16 @@ describe('Behavior with no folder open', function() {
 	});
 
 	it('should display modal error message when first command run', async function () {
+		console.log(`The log file: ${logFileName}`);
 		await workbench.executeCommand('gvQLC: View Quiz Questions');
+		console.log(Date.now());
+		await new Promise(resolve => setTimeout(resolve, 5_000));
+		console.log(Date.now());
 		logAllNotifications();
 
 		await waitForNotification(NotificationType.Error, (message: string) => {
 			return message.indexOf('gvQLC requires a workspace folder to be open. (modal)') >= 0;
-		});
+		}, 7_000);
 	});
 
 	it('should display non-modal error message when second command run', async function () {
