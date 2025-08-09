c2df8348359c4771ecaf6401b1c30cc2340819f9
Zachary Kurmas <kurmasz@gvsu.edu>
2025-08-08 20:56:59 -0400

    More paganiation tests.

--- test/system/viewQuizQuestions.test.ts
+++ test/system/viewQuizQuestions.test.ts
@@ -10,22 +10,24 @@
  * (C) 2025 Zachary Kurmas
  * *********************************************************************************/
 
-import { EditorView, Workbench, WebDriver, WebView, VSBrowser, NotificationType } from 'vscode-extension-tester';
+import { Workbench, WebDriver, WebView, VSBrowser, NotificationType } from 'vscode-extension-tester';
 import { By, until, WebElement } from 'selenium-webdriver';
 import { logAllNotifications, waitForNotification } from '../helpers/systemHelpers';
+import {verifyQuestionDisplayed, verifySummaryDisplayed, setUpQuizQuestionWebView, GREEN, YELLOW} from '../helpers/questionViewHelpers';
+
 import { expect } from 'chai';
 import * as path from 'path';
 
 describe('viewQuizQuestions', function () {
     let driver: WebDriver;
-    let workbench: Workbench;
     let view: WebView;
     let summaryContainer: WebElement;
 
-    const GREEN = 'rgba(0, 128, 0, 1)';
-    const YELLOW = 'rgba(255, 255, 0, 1)';
-
     this.timeout(150_000);
+
+    after(async function() {
+        await driver.switchTo().defaultContent();
+    });
 
     /////////////////////////
     //
@@ -42,7 +44,7 @@ describe('viewQuizQuestions', function () {
             await driver.wait(until.elementIsVisible(element), 5_000);
         });
 
-        workbench = new Workbench();
+        const workbench = new Workbench();
         await workbench.wait();
 
         // Run the command
@@ -57,54 +59,8 @@ describe('viewQuizQuestions', function () {
     //
     ///////////////////////// 
     it('opens the folder, runs the command and shows the title and total questions', async () => {
-
-        // Open the folder
         const folder = path.resolve(__dirname, '..', '..', '..', 'test-fixtures', 'cis371_server');
-
-        await VSBrowser.instance.openResources(folder, async () => {
-            const selector = By.css('[aria-label="Explorer Section: cis371_server"]');
-            const element = await driver.wait(until.elementLocated(selector), 10_000);
-            await driver.wait(until.elementIsVisible(element), 5_000);
-        });
-
-        workbench = new Workbench();
-        await workbench.wait();
-        console.log(await new EditorView().getOpenEditorTitles());
-
-
-        // Run the command
-        await workbench.executeCommand('gvQLC: View Quiz Questions');
-        await new Promise(res => setTimeout(res, 10000)); // crude but useful
-
-        // const tabs = await driver.findElements(By.css('.tab-label'));
-        // console.log('Tabs 1:', await Promise.all(tabs.map(t => t.getText())));
-
-        const tab = await driver.wait(until.elementLocated(By.css('[aria-label="View Quiz Questions"]')), 15_000);
-        await driver.wait(until.elementIsVisible(tab), 5_000);
-
-        // Switch to the frame containing the new view
-        view = new WebView();
-        await view.switchToFrame();
-
-        // Check the title and number of questions.
-        await driver.wait(until.elementLocated(By.css('h1')));
-        const element = await view.findWebElement(By.css('h1'));
-        expect(await element.getText()).has.string('All Quiz Questions');
-        const element2 = await view.findWebElement(By.css('.total-count'));
-        expect(await element2.getText()).has.string('Total Questions: 12');
-    });
-
-    it('defaults to displaying 15 rows', async () => {
-        // Make sure the pagination is set to 15. Otherwise, tests further down will break.
-        const element3 = await view.findWebElement(By.css('#rowsPerPage option[value="15"]'));
-        console.log('Attribute value: ');
-        console.log(await element3.getAttribute('selected'));
-        expect(await element3.getAttribute('selected') !== null);
-    });
-
-    it('shows all 12 rows fit on one page', async () => {
-        const element = await view.findWebElement(By.css('#totalPagesDisplay'));
-        expect(await element.getText()).to.equal("1");
+        ({view, summaryContainer} = await setUpQuizQuestionWebView(driver, folder, '12'));
     });
 
     it('displays the first queston', async () => {
@@ -187,7 +143,7 @@ describe('viewQuizQuestions', function () {
         expect(await button.isDisplayed()).to.be.true;
         await button.click();
 
-        summaryContainer = await view.findWebElement(By.css('#summaryTableContainer'));
+       // summaryContainer = await view.findWebElement(By.css('#summaryTableContainer'));
         expect(await summaryContainer.isDisplayed()).to.be.true;
 
         const header = await summaryContainer.findElement(By.css('h2'));
@@ -247,178 +203,4 @@ describe('viewQuizQuestions', function () {
         const container = await view.findWebElement(By.css('#summaryTableContainer'));
         expect(await container.isDisplayed()).to.be.false;
     });
-
-    /////////////////////////
-    //
-    // Paganation
-    //
-    /////////////////////////
-    it('Only shows first ten when paganation set to 10', async () => {
-        const rowsSelect = await driver.findElement(By.id("rowsPerPage"));
-        await rowsSelect.click();
-        const option10 = await rowsSelect.findElement(By.css('option[value="10"]'));
-        await option10.click();
-    });
-
-    it('shows there are now two pages', async () => {
-        const element = await view.findWebElement(By.css('#totalPagesDisplay'));
-        expect(await element.getText()).to.equal("2");
-    });
-
-    it('displays the first queston (when two pages)', async () => {
-        const expected = `                while line := file.readline():
-                    socket.send_text_line(line)`;
-
-        await verifyQuestionDisplayed(view, {
-            rowIndex: 0,
-            rowLabel: '1a',
-            color: GREEN,
-            file: 'antonio/my_http_server.py',
-            code: expected,
-            question: "Explain the difference between `=` and `:=`",
-        });
-    });
-
-    it('displays the ninth queston (when two pages)', async () => {
-        const expected = `        if path.endswith((".jpeg", ".jpg", ".png", ".gif", ".ico", ".pdf")):
-            read_mode = "rb"
-
-        file_size = os.path.getsize(path)
-        with open(path, read_mode) as file:
-            socket.send_text_line("HTTP/1.0 200 OK")
-            socket.send_text_line(f"Content-Type: {content_type}")
-            socket.send_text_line(f"Content-Length: {file_size}")
-            socket.send_text_line(f"Connection: close")
-            socket.send_text_line("")
-
-            if read_mode == 'rb':
-                socket.send_binary_data_from_file(file, file_size)
-
-            else:
-                while line := file.readline():
-                    socket.send_text_line(line)`;
-
-        await verifyQuestionDisplayed(view, {
-            rowIndex: 9,
-            rowLabel: '7a',
-            color: YELLOW,
-            file: 'neptune_man/my_http_server.py',
-            code: expected,
-            question: "How would read mode ever be anything but `rb`?",
-        });
-    });
-
-    it('Does not display the 11th row', async () => {
-        const row = await view.findWebElement(By.css(`#row-10`));
-        const cells = await row.findElements(By.css('td'));
-        expect(await row.isDisplayed()).to.be.false;
-    });
-
-    it('Does not display the 12th row', async () => {
-        const row = await view.findWebElement(By.css(`#row-11`));
-        expect(await row.isDisplayed()).to.be.false;
-    });
-
-    it('Advances to the next page when I choose page 2', async () => {
-        // Finds: <button class="page-ban">2</button>
-        const button2 = await driver.findElement(By.xpath("//button[normalize-space()='2']"));
-        await button2.click();
-
-        const row0 = await view.findWebElement(By.css(`#row-0`));
-        expect(await row0.isDisplayed()).to.be.false;
-
-        const row9 = await view.findWebElement(By.css(`#row-9`));
-        expect(await row9.isDisplayed()).to.be.false;
-
-        const row10 = await view.findWebElement(By.css(`#row-10`));
-        expect(await row10.isDisplayed()).to.be.true;
-
-        await verifyQuestionDisplayed(view, {
-            rowIndex: 11,
-            rowLabel: '8b',
-            color: GREEN,
-            file: 'uncle_bob/my_http_server.py',
-            code: " if os.path.isdir(path) or os.path.isdir(f'{path}/') or path[-1] == '/':",
-            question: "What is the significance of `path[-1] == '/'? What exactly is being checked, and what does that mean at a high level? ",
-        });
-    });
-
-
-    it('Displays summary for all rows, even if not all rows are currently displayed', async () => {
-        const button = await view.findWebElement(By.id('toggleSummaryBtn'));
-        expect(await button.isDisplayed()).to.be.true;
-        await button.click();
-
-        summaryContainer = await view.findWebElement(By.css('#summaryTableContainer'));
-        expect(await summaryContainer.isDisplayed()).to.be.true;
-
-        const header = await summaryContainer.findElement(By.css('h2'));
-        expect(await header.getText()).to.equal('Student Question Summary');
-
-        // first row
-        verifySummaryDisplayed(summaryContainer, {
-            name: 'antonio',
-            questionCount: 2,
-            color: GREEN,
-            hasQuestions: true
-        });
-
-        // last row
-        verifySummaryDisplayed(summaryContainer, {
-            name: 'uncle_bob',
-            questionCount: 1,
-            color: YELLOW,
-            hasQuestions: true
-        });
-    });
 });
-
-type QuestionData = {
-    rowIndex: number,
-    rowLabel: string,
-    color: string,
-    file: string,
-    code: string,
-    question: string
-};
-
-async function verifyQuestionDisplayed(view: WebView, questionData: QuestionData) {
-    const row = await view.findWebElement(By.css(`#row-${questionData.rowIndex}`));
-    const cells = await row.findElements(By.css('td'));
-    expect(await row.isDisplayed()).to.be.true;
-
-    expect(await cells[0].getText()).to.equal(questionData.rowLabel);
-    expect(await cells[0].getCssValue('background-color')).to.equal(questionData.color);
-
-    expect(await cells[1].getText()).to.equal(questionData.file);
-
-    const codeArea = await cells[2].findElement(By.css('textarea'));
-    const actualText = await codeArea.getText();
-    if (actualText !== questionData.code) {
-        console.log("Expected");
-        console.log(questionData.code);
-        console.log("Actual");
-        console.log(await codeArea.getText());
-    }
-    expect(actualText).to.equal(questionData.code);
-
-    const questionArea = await cells[3].findElement(By.css('textArea'));
-    expect(await questionArea.getText()).to.equal(questionData.question);
-}
-
-type SummaryData = {
-    name: string,
-    questionCount: number,
-    hasQuestions: boolean,
-    color: string,
-};
-
-async function verifySummaryDisplayed(container: WebElement, summaryData: SummaryData) {
-    const row = container.findElement(By.xpath(`.//tr[td[1][contains(normalize-space(.), "${summaryData.name}")]]`));
-    expect(await row.getCssValue('background-color')).to.equal(summaryData.color);
-
-    const cells = await row.findElements(By.css('td'));
-    expect(await cells[1].getText()).to.equal(summaryData.questionCount.toString());
-    const expected = summaryData.hasQuestions ? '✓' : '';
-    expect(await cells[2].getText()).to.equal(expected);
-}
---
+++ test/system/viewQuizQuestionsPagination.test.ts
@@ -0,0 +1,234 @@
+/************************************************************************************
+ * 
+ * viewQuizQuestionsPagination.test.ts
+ * 
+ * Test the pagination aspect of the viewQuizQuestions command.
+ * 
+ * IMPORTANT: Remember: VSCode and the extension are _not_ re-set between tests.
+ * these tests must run in order.
+ * 
+ * (C) 2025 Zachary Kurmas
+ * *********************************************************************************/
+
+import { WebDriver, WebView, VSBrowser } from 'vscode-extension-tester';
+import { By, until, WebElement } from 'selenium-webdriver';
+import { verifyQuestionDisplayed, verifySummaryDisplayed, setUpQuizQuestionWebView, GREEN, YELLOW } from '../helpers/questionViewHelpers';
+
+import { expect } from 'chai';
+import * as path from 'path';
+
+describe('viewQuizQuestions pagination', function () {
+    let driver: WebDriver;
+    let view: WebView;
+    let summaryContainer: WebElement;
+
+    this.timeout(150_000);
+
+    after(async function () {
+        await driver.switchTo().defaultContent();
+    });
+
+    it('opens the folder and runs the command', async () => {
+        driver = VSBrowser.instance.driver;
+        const folder = path.resolve(__dirname, '..', '..', '..', 'test-fixtures', 'cis371_server');
+        ({ view, summaryContainer } = await setUpQuizQuestionWebView(driver, folder, '12'));
+    });
+
+    it('defaults to displaying 15 rows', async () => {
+        // Make sure the pagination is set to 15. Otherwise, tests further down will break.
+        const element3 = await view.findWebElement(By.css('#rowsPerPage option[value="15"]'));
+        expect(await element3.getAttribute('selected') !== null);
+    });
+
+    it('shows that there is only one page', async () => {
+        const element = await view.findWebElement(By.css('#totalPagesDisplay'));
+        expect(await element.getText()).to.equal("1");
+
+        const pageNumberContainer = await view.findWebElement(By.css('#pageNumbers'));
+        const allPageNumbers = await pageNumberContainer.findElements(By.css('.page-btn'));
+        expect(allPageNumbers.length).to.equal(1);
+        expect(await allPageNumbers[0].getText()).to.equal("1");
+    });
+
+    it('disables the navigation buttons', async () => {
+        verifyButtonDisabled('#firstPageBtn');
+        verifyButtonDisabled('#prevPageBtn');
+        verifyButtonDisabled('#nextPageBtn');
+        verifyButtonDisabled('#lastPageBtn');
+    });
+
+    it('displays the 1st row', async () => {
+        const row = await view.findWebElement(By.css(`#row-0`));
+        const cells = await row.findElements(By.css('td'));
+        expect(await row.isDisplayed()).to.be.true;
+    });
+
+    it('displays the last row', async () => {
+        const row = await view.findWebElement(By.css(`#row-11`));
+        expect(await row.isDisplayed()).to.be.true;
+    });
+
+    ////////////////////////////////
+    //
+    // Set pagination to 10
+    //
+    ////////////////////////////////
+    it('Only shows first ten when paganation set to 10', async () => {
+        const rowsSelect = await driver.findElement(By.id("rowsPerPage"));
+        await rowsSelect.click();
+        const option10 = await rowsSelect.findElement(By.css('option[value="10"]'));
+        await option10.click();
+    });
+
+    it('shows there are now two pages', async () => {
+        const element = await view.findWebElement(By.css('#totalPagesDisplay'));
+        expect(await element.getText()).to.equal("2");
+
+        const pageNumberContainer = await view.findWebElement(By.css('#pageNumbers'));
+        const allPageNumbers = await pageNumberContainer.findElements(By.css('.page-btn'));
+        expect(allPageNumbers.length).to.equal(2);
+        expect(await allPageNumbers[0].getText()).to.equal("1");
+        expect(await allPageNumbers[0].getText()).to.equal("1");
+    });
+
+    it('disables "previous" buttons when on page 1 of 2', async() => {
+        verifyButtonDisabled('#firstPageBtn');
+        verifyButtonDisabled('#prevPageBtn');
+    });
+
+    it('enables "next" buttons when on page 1 of 2', async() => {
+        verifyButtonEnabled('#nextPageBtn');
+        verifyButtonEnabled('#lastPageBtn');
+    });
+
+    it('enables "previous" buttons when on page 2 of 2', async() => {
+        verifyButtonEnabled('#firstPageBtn');
+        verifyButtonEnabled('#prevPageBtn');
+    });
+
+    it('disables "next" buttons when on page 2 of 2', async() => {
+        verifyButtonDisabled('#nextPageBtn');
+        verifyButtonDisabled('#lastPageBtn');
+    });
+
+    it('displays the first queston (when two pages)', async () => {
+        const expected = `                while line := file.readline():
+                    socket.send_text_line(line)`;
+
+        await verifyQuestionDisplayed(view, {
+            rowIndex: 0,
+            rowLabel: '1a',
+            color: GREEN,
+            file: 'antonio/my_http_server.py',
+            code: expected,
+            question: "Explain the difference between `=` and `:=`",
+        });
+    });
+
+    it('displays the ninth queston (when two pages)', async () => {
+        const expected = `        if path.endswith((".jpeg", ".jpg", ".png", ".gif", ".ico", ".pdf")):
+            read_mode = "rb"
+
+        file_size = os.path.getsize(path)
+        with open(path, read_mode) as file:
+            socket.send_text_line("HTTP/1.0 200 OK")
+            socket.send_text_line(f"Content-Type: {content_type}")
+            socket.send_text_line(f"Content-Length: {file_size}")
+            socket.send_text_line(f"Connection: close")
+            socket.send_text_line("")
+
+            if read_mode == 'rb':
+                socket.send_binary_data_from_file(file, file_size)
+
+            else:
+                while line := file.readline():
+                    socket.send_text_line(line)`;
+
+        await verifyQuestionDisplayed(view, {
+            rowIndex: 9,
+            rowLabel: '7a',
+            color: YELLOW,
+            file: 'neptune_man/my_http_server.py',
+            code: expected,
+            question: "How would read mode ever be anything but `rb`?",
+        });
+    });
+
+    it('Does not display the 11th row', async () => {
+        const row = await view.findWebElement(By.css(`#row-10`));
+        const cells = await row.findElements(By.css('td'));
+        expect(await row.isDisplayed()).to.be.false;
+    });
+
+    it('Does not display the 12th row', async () => {
+        const row = await view.findWebElement(By.css(`#row-11`));
+        expect(await row.isDisplayed()).to.be.false;
+    });
+
+    it('Advances to page 2 by number button', async () => {
+        // Finds: <button class="page-ban">2</button>
+        const button2 = await driver.findElement(By.xpath("//button[normalize-space()='2']"));
+        await button2.click();
+
+        const row0 = await view.findWebElement(By.css(`#row-0`));
+        expect(await row0.isDisplayed()).to.be.false;
+
+        const row9 = await view.findWebElement(By.css(`#row-9`));
+        expect(await row9.isDisplayed()).to.be.false;
+
+        const row10 = await view.findWebElement(By.css(`#row-10`));
+        expect(await row10.isDisplayed()).to.be.true;
+
+        await verifyQuestionDisplayed(view, {
+            rowIndex: 11,
+            rowLabel: '8b',
+            color: GREEN,
+            file: 'uncle_bob/my_http_server.py',
+            code: " if os.path.isdir(path) or os.path.isdir(f'{path}/') or path[-1] == '/':",
+            question: "What is the significance of `path[-1] == '/'? What exactly is being checked, and what does that mean at a high level? ",
+        });
+    });
+
+
+    it('Displays summary for all rows, even if not all rows are currently displayed', async () => {
+        const button = await view.findWebElement(By.id('toggleSummaryBtn'));
+        expect(await button.isDisplayed()).to.be.true;
+        await button.click();
+
+        summaryContainer = await view.findWebElement(By.css('#summaryTableContainer'));
+        expect(await summaryContainer.isDisplayed()).to.be.true;
+
+        const header = await summaryContainer.findElement(By.css('h2'));
+        expect(await header.getText()).to.equal('Student Question Summary');
+
+        // first row
+        verifySummaryDisplayed(summaryContainer, {
+            name: 'antonio',
+            questionCount: 2,
+            color: GREEN,
+            hasQuestions: true
+        });
+
+        // last row
+        verifySummaryDisplayed(summaryContainer, {
+            name: 'uncle_bob',
+            questionCount: 1,
+            color: YELLOW,
+            hasQuestions: true
+        });
+    });
+
+    
+
+
+
+    async function verifyButtonEnabled(id: string, enabled: boolean = true) {
+        const button = await view.findWebElement(By.css(id));
+        const firstIsDisabled = await button.getAttribute('disabled') !== null;
+        expect(firstIsDisabled).to.equal(enabled);
+    }
+
+    async function verifyButtonDisabled(id: string) {
+        verifyButtonEnabled(id, false);
+    }
+}); // end describe
